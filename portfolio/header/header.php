82fdebed5f5ca645be1b89e9db26c7a90daac122
Aleksandr Shakhray <shakhray.a1991@gmail.ru>
2023-10-09 17:12:06 +0500

    portfolio

---
+++ portfolio/header/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Kanit&family=Michroma&family=Noto+Sans+KR&family=Roboto+Condensed:ital,wght@0,400;1,700&family=Roboto:wght@300;400&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:opsz@6..12&display=swap" rel="stylesheet">
+    <title>Document</title>
+</head>
+
+<body>
+    <header>
+        <div class="navigation">
+            <a href="index.php">
+                <div class="logotip"></div>
+            </a>
+            <nav class="nav">
+                <div><a href="cycle.php" class="">Циклы</a> </div>
+                <div><a href="array.php" class="">Массивы</a> </div>
+                <div><a href="functions.html" class="">Функции</a> </div>
+                <div><a href="string.html" class="">Строки</a> </div>
+                <div><a href="#" class="">#</a> </div>
+            </nav>
+        </div>
+    </header>
---
+++ portfolio/index.php
@@ -0,0 +1,76 @@
+<?php require_once 'header/header.php' ?>
+<main>
+    <div class="name">
+        <h1>Шахрай Александр</h1>
+    </div>
+    <div class="about-container-grid">
+        <div class="photo">
+        </div>
+        <div class="about_me">
+            <h2>Обо мне</h2>
+            <p>Всем привет. Меня зовут Александр. Мне 31 год. </p>
+            <i>
+                <p>Я начинаю путь в новое для себя направление веб-разработки и хочу рассказать о том, чем
+                    занимаюсь:
+                </p>
+            </i>
+            <ul>
+                <li>
+                    Моя основная сфера деятельности связана с ритейлом. В послледнее время работал менеджером по
+                    логистике.
+                </li>
+                <li>
+                    Любимые увлечения - это чтение книг, спорт, видеоигры, выезды на природу.
+                </li>
+                <li>
+                    Программирование изучаю c 2022 года. Пробовал сам изучать Java, HTML, CSS. Надеюсь в
+                    дальнейшем
+                    сделать программирование своим основным видом деятельности. Выбирал между вашем курсом и
+                    компанией Хекслет.
+                    После окончания курса хотел бы устроиться в Факт или в компании партнёры на программиста.
+                </li>
+            </ul>
+        </div>
+        <div class="feedback">
+            <h2>О курсе</h2>
+            <p>
+                Мнение о курсе сложно составить по нескольким урокам, на данный момент могу отметить небольшое
+                количество учащихся, что позволит проработать индивидуально ошибки. Понравился формат обучения и
+                внимательность преподавателя к каждому человеку, заставляет включаться в процесс. Надеюсь, что и
+                последующие уроки будут проходить для меня комфортно, а материал усваиваться легко и быстро. <strong>P.S Тяжело даются математические задачи.</strong>
+            </p>
+        </div>
+    </div>
+    </section>
+    <section class="kino">
+        <div class="kino-name">
+            <h1>Мои любимые фильмы</h1>
+            <hr>
+        </div>
+        <div class="kino-flex-container">
+            <img src="img/truman.jpg" class="item" alt="#" height="350px" width="250px">
+            <img src="img/fightclub_1.jpg" class="item" alt="#" height="350px" width="250px">
+            <img src="img/Silicon_Valley.png" class="item" alt="#" height="350px" width="250px">
+            <img src="img/mrrobot_21.jpg" class="item" alt="#" height="350px" width="250px">
+        </div>
+    </section>
+    <section class="book">
+        <h1>Мои любимые книги</h1>
+        <hr>
+        <div class="book-grid-container">
+            <div class="">
+                <img src="img/tekst.jpg" class="item" alt="#" height="300px" width="250px">
+            </div>
+            <div class="">
+                <img src="img/2.jpg" class="item" alt="#" height="300px" width="250px">
+            </div>
+            <div class="">
+                <img src="img/3.jpeg" class="item" alt="#" height="300px" width="250px">
+            </div>
+            <div class="">
+                <img src="img/4.jpg" class="item" alt="#" height="300px" width="250px">
+            </div>
+        </div>
+    </section>
+   
+ <?php require_once 'footer/footer.php' ?>
