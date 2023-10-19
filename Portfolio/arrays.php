fd65ed089ebaa5a9259a191cd41ed5593014e6ec
AlexFilonenko88 <af88_88@mail.ru>
2023-10-19 16:14:32 +0300

    add вусшdecisionray, string

--- Portfolio/arrays.php
+++ Portfolio/arrays.php
@@ -2,7 +2,8 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <link rel="stylesheet" href="css/style_task.css">
@@ -14,42 +15,42 @@
         <div class="wrapperOne">
             <div>Задание 1</div>
             <?
-                $result = 0;
-                $arr = [50, 45, 40, 35, 30];
+            $result = 0;
+            $arr = [50, 45, 40, 35, 30];
 
-                foreach ($arr as $elem){
-                    $result += $elem;
-                };
+            foreach ($arr as $elem) {
+                $result += $elem;
+            };
 
-                echo $result;
+            echo $result;
             ?>
         </div>
         <div class="wrapperTwo">
             <div>Задание 2</div>
             <?
-                $arr = [];
+            $arr = [];
 
-                for ($i=0; $i<=4; $i++){
-                    $number = mt_rand(0, 5);
-                    echo $arr[$i] = $number;
-                };
+            for ($i = 0; $i <= 4; $i++) {
+                $number = mt_rand(0, 5);
+                echo $arr[$i] = $number;
+            };
 
-                echo '<br/>';
-                echo '<br/>';
+            echo '<br/>';
+            echo '<br/>';
 
-                for ($i=count($arr)-1; $i>=0; $i--){
-                    echo $arr[$i];
-                };
+            for ($i = count($arr) - 1; $i >= 0; $i--) {
+                echo $arr[$i];
+            };
             ?>
         </div>
         <div class="wrapperTree">
             <div>Задание 3</div>
             <?
-                $arr = ['1.jpeg', '2.jpg'];
+            $arr = ['1.jpeg', '2.jpg'];
 
-                $rand_img = mt_rand(0, count($arr)-1);
+            $rand_img = mt_rand(0, count($arr) - 1);
 
-                echo "<img src='./img/$arr[$rand_img]' width='100px' alt=''";
+            echo "<img src='./img/$arr[$rand_img]' width='100px' alt=''";
             ?>
         </div>
     </div>
@@ -57,40 +58,75 @@
 <div class="wrapper">
     <h1>Слайд 16</h1>
     <div class="wrapperOne">
-        <div>Задание 1</div>
-            <?php
-            $islands = ["Cuba","Trinidad","Jamaica","Haiti"];
-            $months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-            $journal = array();
-            $arr_new = array();
-            foreach($islands as $nameIsland){
-                foreach($months as $nameMonth){
-                    $celsius = mt_rand(15,40);
-                    $journal[$nameIsland][$nameMonth] = $celsius;
-                    if (array_key_exists($nameMonth,$arr_new)){
-                        if ($arr_new[$nameMonth] < $celsius){
-                            $arr_new[$nameMonth] = $celsius;
-                        }
-                    }
-                    else{
+        <div>Задание 5</div>
+        <?php
+        $islands = ["Cuba", "Trinidad", "Jamaica", "Haiti"];
+        $months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        $journal = array();
+        $arr_new = array();
+        foreach ($islands as $nameIsland) {
+            foreach ($months as $nameMonth) {
+                $celsius = mt_rand(15, 40);
+                $journal[$nameIsland][$nameMonth] = $celsius;
+                if (array_key_exists($nameMonth, $arr_new)) {
+                    if ($arr_new[$nameMonth] < $celsius) {
                         $arr_new[$nameMonth] = $celsius;
                     }
+                } else {
+                    $arr_new[$nameMonth] = $celsius;
                 }
             }
-            echo "<pre>";
-            print_r($journal);
-            echo "Массив максимальных температур для островов : <br>";
-            print_r($arr_new);
-            echo "</pre>";
+        }
+        echo "<pre>";
+        print_r($journal);
+        echo "Массив максимальных температур для островов : <br>";
+        print_r($arr_new);
+        echo "</pre>";
 
-            ?>
+        ?>
     </div>
     <div class="wrapperTwo">
-        <div>Задание 2</div>
+        <div>Задание 6</div>
+        <?
+        function maximumTemperature ($array)
+        {
+            foreach ($array as $key => $value) {
+                $max = 0;
+                foreach ($value as $new_key => $new_value) {
+                    if ($new_value > $max) {
+                        $max = $new_value;
+                    }
+                }
+                echo "Максимальная температура $key  =   $max  <br/>";
+            }
+        };
 
+        $array = [
+            'Июнь' => [
+                'Куба' => mt_rand(20, 60),
+                'Тринидад' => mt_rand(20, 60),
+                'Ямайка' => mt_rand(20, 60),
+                'Гаити' => mt_rand(20, 60)
+            ],
+            'Июль' => [
+                'Куба' => mt_rand(20, 60),
+                'Тринидад' => mt_rand(20, 60),
+                'Ямайка' => mt_rand(20, 60),
+                'Гаити' => mt_rand(20, 60)
+            ],
+            'Август' => [
+                'Куба' => mt_rand(20, 60),
+                'Тринидад' => mt_rand(20, 60),
+                'Ямайка' => mt_rand(20, 60),
+                'Гаити' => mt_rand(20, 60)
+            ]
+        ];
+
+        maximumTemperature($array);
+        ?>
     </div>
     <div class="wrapperTree">
-        <div>Задание 3</div>
+        <div>Задание 7</div>
 
     </div>
 </div>
--- Portfolio/string.php
+++ Portfolio/string.php
@@ -14,61 +14,125 @@
         <div class="wrapperOne">
             <div>Задание 1</div>
             <?
-                $str = 'fact';
-                echo $result = str_replace('f', 'F', $str);
+            $str = 'fact';
+            echo $result = str_replace('f', 'F', $str);
             ?>
         </div>
         <div class="wrapperTwo">
             <div>Задание 2</div>
             <?
-                $str = 'Филоненко Алексей Андреевич';
+            $str = 'Филоненко Алексей Андреевич';
 
-                $name = mb_substr($str, 10, 7);
-                $surname = mb_substr($str, 0, 9);
-                echo $name . " " . $surname;
+            $name = mb_substr($str, 10, 7);
+            $surname = mb_substr($str, 0, 9);
+            echo $name . " " . $surname;
             ?>
         </div>
         <div class="wrapperTree">
             <div>Задание 3</div>
             <?
-                $str = "Привет, мИр";
+            $str = "Привет, мИр";
 
-                $arrSymbol = mb_str_split($str, 1);
-                $count = 0;
+            $arrSymbol = mb_str_split($str, 1);
+            $count = 0;
 
-                foreach ($arrSymbol as $value){
-                    if ($value == 'и' || $value == 'И'){
-                        $count += 1;
-                    }
+            foreach ($arrSymbol as $value){
+                if ($value == 'и' || $value == 'И'){
+                    $count += 1;
                 }
+            }
 
-                echo $count;
+            echo $count;
             ?>
         </div>
         <div class="wrapperTree">
             <div>Задание 4</div>
-                <?
-                    $str = 'html css php';
+            <?
+            $str = 'html css php';
 
-                    echo $html = mb_substr($str, 0,4);
-                    echo '<br>';
-                    echo $css = mb_substr($str, 5,3);
-                    echo '<br>';
-                    echo $php = mb_substr($str, 9,3);
-                ?>
+            echo $html = mb_substr($str, 0,4);
+            echo '<br>';
+            echo $css = mb_substr($str, 5,3);
+            echo '<br>';
+            echo $php = mb_substr($str, 9,3);
+            ?>
         </div>
         <div class="wrapperTree">
             <div>Задание 5</div>
             <?
-                $str = 'dsvvvvdv.png';
+            $str = 'dsvvvvdv.png';
 
-                $substr = substr($str, -4,4);
+            $substr = substr($str, -4,4);
 
-                if ($substr == '.png'){
-                    echo 'да';
-                }else {
-                    echo 'нет';
+            if ($substr == '.png'){
+                echo 'да';
+            }else {
+                echo 'нет';
+            }
+            ?>
+        </div>
+    </div>
+
+<!--   ______ -->
+
+    <div class=" wrapper">
+        <h1>Слайд 23</h1>
+        <div class="wrapperOne">
+            <div>Задание 1</div>
+            <?
+                function str ($string)
+                {
+                    echo (strlen($string) > 5) ? mb_substr($string, 0, 5) . "..." : $string;
                 }
+
+                $string = "String";
+
+                echo str($string);
+            ?>
+        </div>
+        <div class="wrapperTwo">
+            <div>Задание 2</div>
+            <?
+
+            ?>
+        </div>
+        <div class="wrapperTree">
+            <div>Задание 3</div>
+            <?
+                function positionLastLetter ($string)
+                {
+                    echo "позиция последней буквы 'b' = " . strrpos($string, "b");
+                }
+
+                $string = "abc abc abc";
+
+                positionLastLetter($string);
+            ?>
+        </div>
+        <div class="wrapperTree">
+            <div>Задание 4</div>
+            <?
+                function wordSeparateArrayElement ($string) {
+                    print_r(explode(' ',$string));
+                }
+
+                $string = "html css php";
+
+                wordSeparateArrayElement ($string);
+            ?>
+        </div>
+        <div class="wrapperTree">
+            <div>Задание 5</div>
+            <?
+                function numberDaysBetweenDates ($now, $my_date) {
+                    $date = $now - $my_date;
+                    echo intdiv($date , (60 * 60 * 24));
+                }
+
+                $now = strtotime("2021-11-29");
+                $my_date = strtotime("1988-11-02");
+
+                numberDaysBetweenDates($now, $my_date);
             ?>
         </div>
     </div>
