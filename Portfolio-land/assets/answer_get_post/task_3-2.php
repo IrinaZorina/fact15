fb281cc3b7e64c48cf870b1d2597a10b9dcfd4b9
Andrey Khaustov <skpalatka@gmail.com>
2023-10-23 17:06:53 +0500

    edit files portfolio-land

--- Portfolio-land/assets/answer_get_post/task_3-2.php
+++ Portfolio-land/assets/answer_get_post/task_3-2.php
@@ -1,175 +1,191 @@
 <?php
-print_r($_POST);
-        $pageResult = 'task_3-2_Good.php';
-        header('location: ' . $pageResult);
+$formSum = array_sum($_POST);
+if ($formSum > 15)
+{
+    $str = 'У Вас покладистый характер';
+}
+if ($formSum > 8 && $formSum < 15)
+{
+    $str = 'Вы не лишены недостатков, но с вами можно ладить';
+}
+if ($formSum < 8) 
+{
+    $str = 'Вашим друзьям можно посочувствовать';
+}
+if ($formSum == 0) 
+{
+    $str = 'Пройдите тестирование';
+}
+/*         $pageResult = 'task_3-2_Good.php';
+        header('location: ' . $pageResult); */
 ?>
 
 
 
-
-
+<link rel="stylesheet" href="/css/main.css" />
+<div class="resultTask3_2"> <?php  echo $str ?> </div>
 <form action="/assets/answer_get_post/task_3-2.php" method="POST" class="form3_2">
 
 <p>
     <p><strong>Вопрос №1</strong></p>
-    <input id="yes" type="radio" name="question1" value="yes" checked>
+    <input id="yes" type="radio" name="question1" value="0" checked>
     <label for="yes">Да</label>
-    <input id="no" type="radio" name="question1" value="no">
+    <input id="no" type="radio" name="question1" value="1">
     <label for="no">Нет</label>
 </p>
 
 <p>
     <p><strong>Вопрос №2</strong></p>
-    <input id="yes" type="radio" name="question2" value="yes" checked>
+    <input id="yes" type="radio" name="question2" value="0" checked>
     <label for="yes">Да</label>
-    <input id="no" type="radio" name="question2" value="no">
+    <input id="no" type="radio" name="question2" value="1">
     <label for="no">Нет</label>
 </p>
 
 <p>
     <p><strong>Вопрос №3</strong></p>
-    <input id="yes" type="radio" name="question3" value="yes" checked>
+    <input id="yes" type="radio" name="question3" value="1" checked>
     <label for="yes">Да</label>
-    <input id="no" type="radio" name="question3" value="no">
+    <input id="no" type="radio" name="question3" value="0">
     <label for="no">Нет</label>
 </p>
 
 <p>
     <p><strong>Вопрос №4</strong></p>
-    <input id="yes" type="radio" name="question4" value="yes" checked>
+    <input id="yes" type="radio" name="question4" value="0" checked>
     <label for="yes">Да</label>
-    <input id="no" type="radio" name="question4" value="no">
+    <input id="no" type="radio" name="question4" value="1">
     <label for="no">Нет</label>
 </p>
 
 <p>
     <p><strong>Вопрос №5</strong></p>
-    <input id="yes" type="radio" name="question5" value="yes" checked>
+    <input id="yes" type="radio" name="question5" value="0" checked>
     <label for="yes">Да</label>
-    <input id="no" type="radio" name="question5" value="no">
+    <input id="no" type="radio" name="question5" value="1">
     <label for="no">Нет</label>
 </p>
 
 <p>
     <p><strong>Вопрос №6</strong></p>
-    <input id="yes" type="radio" name="question6" value="yes" checked>
+    <input id="yes" type="radio" name="question6" value="0" checked>
     <label for="yes">Да</label>
-    <input id="no" type="radio" name="question6" value="no">
+    <input id="no" type="radio" name="question6" value="1">
     <label for="no">Нет</label>
 </p>
 
 
 <p>
     <p><strong>Вопрос №7</strong></p>
-    <input id="yes" type="radio" name="question7" value="yes" checked>
+    <input id="yes" type="radio" name="question7" value="0" checked>
     <label for="yes">Да</label>
-    <input id="no" type="radio" name="question7" value="no">
+    <input id="no" type="radio" name="question7" value="1">
     <label for="no">Нет</label>
 </p>
 
 <p>
     <p><strong>Вопрос №8</strong></p>
-    <input id="yes" type="radio" name="question8" value="yes" checked>
+    <input id="yes" type="radio" name="question8" value="0" checked>
     <label for="yes">Да</label>
-    <input id="no" type="radio" name="question8" value="no">
+    <input id="no" type="radio" name="question8" value="1">
     <label for="no">Нет</label>
 </p>
 
 <p>
     <p><strong>Вопрос №9</strong></p>
-    <input id="yes" type="radio" name="question9" value="yes" checked>
+    <input id="yes" type="radio" name="question9" value="1" checked>
     <label for="yes">Да</label>
-    <input id="no" type="radio" name="question9" value="no">
+    <input id="no" type="radio" name="question9" value="0">
     <label for="no">Нет</label>
 </p>
 
 
 <p>
     <p><strong>Вопрос №10</strong></p>
-    <input id="yes" type="radio" name="question10" value="yes" checked>
+    <input id="yes" type="radio" name="question10" value="1" checked>
     <label for="yes">Да</label>
-    <input id="no" type="radio" name="question10" value="no">
+    <input id="no" type="radio" name="question10" value="0">
     <label for="no">Нет</label>
 </p>
 
 
 <p>
     <p><strong>Вопрос №11</strong></p>
-    <input id="yes" type="radio" name="question11" value="yes" checked>
+    <input id="yes" type="radio" name="question11" value="0" checked>
     <label for="yes">Да</label>
-    <input id="no" type="radio" name="question11" value="no">
+    <input id="no" type="radio" name="question11" value="1">
     <label for="no">Нет</label>
 </p>
 
 
 <p>
     <p><strong>Вопрос №12</strong></p>
-    <input id="yes" type="radio" name="question12" value="yes" checked>
+    <input id="yes" type="radio" name="question12" value="0" checked>
     <label for="yes">Да</label>
-    <input id="no" type="radio" name="question12" value="no">
+    <input id="no" type="radio" name="question12" value="1">
     <label for="no">Нет</label>
 </p>
 
 
 <p>
     <p><strong>Вопрос №13</strong></p>
-    <input id="yes" type="radio" name="question13" value="yes" checked>
+    <input id="yes" type="radio" name="question13" value="1" checked>
     <label for="yes">Да</label>
-    <input id="no" type="radio" name="question13" value="no">
+    <input id="no" type="radio" name="question13" value="0">
     <label for="no">Нет</label>
 </p>
 
 
 <p>
     <p><strong>Вопрос №14</strong></p>
-    <input id="yes" type="radio" name="question14" value="yes" checked>
+    <input id="yes" type="radio" name="question14" value="1" checked>
     <label for="yes">Да</label>
-    <input id="no" type="radio" name="question14" value="no">
+    <input id="no" type="radio" name="question14" value="0">
     <label for="no">Нет</label>
 </p>
 
 
 <p>
     <p><strong>Вопрос №15</strong></p>
-    <input id="yes" type="radio" name="question15" value="yes" checked>
+    <input id="yes" type="radio" name="question15" value="0" checked>
     <label for="yes">Да</label>
-    <input id="no" type="radio" name="question15" value="no">
+    <input id="no" type="radio" name="question15" value="1">
     <label for="no">Нет</label>
 </p>
 
 
 <p>
     <p><strong>Вопрос №16</strong></p>
-    <input id="yes" type="radio" name="question16" value="yes" checked>
+    <input id="yes" type="radio" name="question16" value="0" checked>
     <label for="yes">Да</label>
-    <input id="no" type="radio" name="question16" value="no">
+    <input id="no" type="radio" name="question16" value="1">
     <label for="no">Нет</label>
 </p>
 
 
 <p>
     <p><strong>Вопрос №17</strong></p>
-    <input id="yes" type="radio" name="question17" value="yes" checked>
+    <input id="yes" type="radio" name="question17" value="0" checked>
     <label for="yes">Да</label>
-    <input id="no" type="radio" name="question17" value="no">
+    <input id="no" type="radio" name="question17" value="1">
     <label for="no">Нет</label>
 </p>
 
 
 <p>
     <p><strong>Вопрос №18</strong></p>
-    <input id="yes" type="radio" name="question18" value="yes" checked>
+    <input id="yes" type="radio" name="question18" value="0" checked>
     <label for="yes">Да</label>
-    <input id="no" type="radio" name="question18" value="no">
+    <input id="no" type="radio" name="question18" value="1">
     <label for="no">Нет</label>
 </p>
 
 
 <p>
     <p><strong>Вопрос №19</strong></p>
-    <input id="yes" type="radio" name="question19" value="yes" checked>
+    <input id="yes" type="radio" name="question19" value="1" checked>
     <label for="yes">Да</label>
-    <input id="no" type="radio" name="question19" value="no">
+    <input id="no" type="radio" name="question19" value="0">
     <label for="no">Нет</label>
 </p>
 
